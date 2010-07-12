7133086f9f143af9dcf7344e70f6cedbf7d6f80f
Johnny Mast <info@webnation.nl>
2010-07-12 21:57:32 +0200

    Recoded the IDv2.4 parser so it would decrease the lines of php code in the class making it more readable.

--- ID3/classes/class.ID3v2.php
+++ ID3/classes/class.ID3v2.php
@@ -60,19 +60,7 @@ class ID3v2 extends ID3_Base implements ID3ParserInterface
 	*/	
 	private $parsedheader = null;
 	
-	/**
- 	* This is the byte positions for ID3v1
- 	* @var array
- 	*/
-	private $headerInfo = array(
-		'TAG'     => array(0, 2),
-		'Title'   => array(3, 32),
-		'Artist'  => array(33, 62),
-		'Album'   => array(63, 92),
-		'Year'    => array(93, 96),
-		'Comment' => array(97, 125),
-		'Genre'   => array(125, 126)	
-	 );
+
 	
 	private $fileinfo = array(
 		'TAG'     => array(0, 2), 
@@ -125,8 +113,6 @@ class ID3v2 extends ID3_Base implements ID3ParserInterface
 		}
 	}
 	
-
-	
 	/**
     * Return the parsed information.
     *
@@ -146,131 +132,6 @@ class ID3v2 extends ID3_Base implements ID3ParserInterface
 		throw new Exception('TODO');
 	}
 	
-	private function parseTextHeader($header = "") {
-		
-		$id    = substr($header, 0, 4);
-		$s     = substr($header, 4, 9); /* Size raw */
-		$f     = ord(  substr($header, 9, 10)  ); /* Flogs raw */
-		
-		
-		$size  = '';
-		$flags = '';
-		
-		for($i = 0; $i < 4; $i++) { 
-			if (ord($s[$i]) == 0) continue;
-			$size .= (int)ord($s[$i]);
-		}
-
-		for($i = 0; $i < 4; $i++) { 
-			if (ord($f[$i]) == 0) continue;
-			$flags .= (int)ord($f[$i]);
-		}
-		
-		$text  = trim( substr($header, 10, $size ) );
-		
-		return array(
-			'ID'    => $id,
-			'SIZE'  => $size,
-			'FLAGS' => $flags,
-			'TEXT'  => $text,
-			'TOTALSIZE' => $size + 10
-		);
-	}
-	
-	private function parseComments($header="")
-    {
-		
-		$id     = substr($header, 0, 4);
-		$l      = substr($header, 4, 9) ; 
-	    $flags  = substr($header, 8, 2);
-		$enc    = substr($header, 10, 1);
-		$lang   = substr($header, 11, 3);
-		
-		$length = '';
-		for($i = 0; $i < 4; $i++) { 
-			if (ord($l[$i]) == 0) continue;
-			$length .= (int)ord($l[$i]);
-		}
-		
-		$length -= 4; /* Why the -4 ? what am i missing ? */
-		
-		//	$short  = trim ( substr($header, 14, $length) ) ;
-		$long   = trim ( substr($header, 14,  $length ) );
-		
-	//	$this->debug('Lang: '.trim($lang));
-	//	$this->debug('Short: '.$short);
-	//	$this->debug('Description: '.$desc);
-		return array(
-			'ID'    => $id,
-			'SIZE'  => $length,
-			'FLAGS' => ord( trim($flags)),
-			'ENCODING' =>  ord( trim($enc)),
-			'LANGUAGE' => $lang,
-			'COMMENT'  => $long,
-			'TOTALSIZE' => 14 + $length
-			);
-	}
-		
-	private function parseAttachmentHeader($header="") {
-		
-		$id    = trim( substr($header, 0, 4) );
-		$s     = trim (substr($header, 4) ); /* Size raw */
-		$f     = ord(  substr($header, 8,10)  ); /* Flogs raw */
-		$e     = ord( substr($header, 10, 11) );
-		$mime  = trim(substr($header, 10, 13));
-		
-		$newpos = 10+strlen($mime) + 1;
-		$type  = trim (substr($header, $newpos) );
-		$desc  = trim(substr($header, $newpos + strlen($type)));
-		$newpos = 10+strlen($mime) +  1 + strlen($type);
-		
-		$img = substr($header, $newpos);
-		//$img = trim($img);
-		$size  = '';
-		$flags = '';
-		$encoding = '';
-
-		for($i = 0; $i < 2; $i++) { 
-			if (ord($e[$i]) == 0) continue;
-			$encoding .= (int)ord($e[$i]);
-		}
-		
-		
-		for($i = 0; $i < 4; $i++) { 
-			if (ord($s[$i]) == 0) continue;
-			$size .= (int)ord($s[$i]);
-		}
-
-		for($i = 0; $i < 4; $i++) { 
-			if (ord($f[$i]) == 0) continue;
-			$flags .= (int)ord($f[$i]);
-		}
-		$img = $type;
-		
-//		$size = 0xd;
-		
-	//	$size = 0x10c1c;
-		$img = substr($img, 0, $size % 4);
-	//	$img[$size] = '\x00';
-		$img = trim($img);
-//		echo '-->'.$size;
-//		exit;
-		return array(
-			'ID'    => $id,
-			'SIZE'  => $size,
-			'FLAGS' => ord( trim($flags)),
-			'MIME' =>  $mime,
-	//		'PICTURETYPE' => $type,
-			'DATA'  => $img,
-		//	'TOTALSIZE' => 10 + $iNullPos +  1 +  $size
-		);
-	}	
-	
-	private function hasPrefix($text, $char)
-	{
-		return ($text[0] == $char);
-	}
-	
 	/**
 	 * This is the parse function for ID3v1. The header information is in the last 128 bytes
 	 */
@@ -279,11 +140,18 @@ class ID3v2 extends ID3_Base implements ID3ParserInterface
 		if ($this->headerpos !== FALSE) {
 			$header = substr($this->getFileData(), $this->headerpos, 10);
 			$info   = array();
-			//echo sprintf('header => %s (size = %d)', $header,mb_strlen($header));
 			
 			foreach(array_keys($this->fileinfo) as $key) {
 				$info[$key] = '';
 			}
+			
+			
+			/*
+			** ID3v2/file identifier   "ID3" 
+			** ID3v2 version           $03 00
+			** ID3v2 flags             %abc00000
+			** ID3v2 size              4 * %0xxxxxxx
+			*/
 			
 			$bitpos = 1;
 			foreach($this->fileinfo as $key => $positions) {
@@ -294,12 +162,10 @@ class ID3v2 extends ID3_Base implements ID3ParserInterface
 				for($i = 0; $i < $length; $i++) {
 					$bit = @$info[$key][$i];
 					$bit = ord($bit);
-				//	$this->debug( sprintf('>%x< (%c) bitpos %d', $bit, $bit, $bitpos) );
 					$bitpos ++;
 				}
 				
 				if ($key == 'VERSION') {
-//					$this->debug( 'Exact version is ID3v2.'.ord($info[$key]) );
 					$info[$key] = sprintf('2.%d', ord($info[$key]));
 				}
 				
@@ -307,84 +173,69 @@ class ID3v2 extends ID3_Base implements ID3ParserInterface
 				if ($key == 'SIZE') {
 					$a = '';
 					for($i= 0; $i < strlen($info[$key]); $i++)
-					  $a = ord($info[$key][$i]);
+					  $a .= ord($info[$key][$i]);
 					
-					$info[$key] = $a;
+					$info[$key] = (int)$a;
 					$info[$key] = sprintf('%d', ord($info[$key]));
 				}
-				//	$this->debug( 'END OF SET<hr />' );
-					
-			    
-			}
-			/*
-			** Extract the media information from the mp3 file
-		
-			$mediainfo = substr($this->getFileData(), 10);
-			$i = 0;
-			while($mediainfo) {
 				
-				if ($this->hasPrefix($mediainfo, 'T')) {
-					$tag = $this->parseTextHeader($mediainfo);
-					$this->parsedheader['TAGS'][$tag['ID']] = $tag;
-					$mediainfo = substr($mediainfo, $tag['TOTALSIZE']);
+				if ($key == 'FLAGS') {
+					$a = '';
+					for($i= 0; $i < strlen($info[$key]); $i++)
+					  $a .= ord($info[$key][$i]);
 					
-			    }
-			
-				if ($this->hasPrefix($mediainfo,'C')) {
-					$tag = $this->parseComments($mediainfo);
-					$this->parsedheader['TAGS'][$tag['ID']] = $tag;
-					$mediainfo = substr($mediainfo, $tag['TOTALSIZE']);
+					$info[$key] = (int)$a;
+					$info[$key] = sprintf('%d', ord($info[$key]));
 				}
-			
-			   elseif ($this->hasPrefix($mediainfo, 'A'))
-			    {
-					$tag = $this->parseAttachmentHeader($mediainfo);
-					$this->parsedheader['TAGS'][$tag['ID']] = $tag;
-					$mediainfo = substr($mediainfo, $tag['TOTALSIZE']);
-				  	break;
-			    } 
-			
-				if ($i == 1500) break;
-					$i++;
 			}
-				*/
+			
 			$identifiers = $this->tags[self::ID3v2][$info['VERSION']];
 			$mediainfo   = substr($this->getFileData(), 10);
-			
-			$i = 0;
-			while($mediainfo) { /* TODO: Should be fixed without while */
-			foreach($identifiers as $ident) {
-				if (substr($mediainfo, 0 , strlen($ident)) == $ident) {
-					if ($this->hasPrefix($mediainfo, 'T')) {
-						$frame = $this->parseTextHeader($mediainfo);
-						//print_r($frame);
 						
-						$this->parsedheader['TAGS'][$frame['ID']] = $frame;
-						$mediainfo = substr($mediainfo, $frame['TOTALSIZE']);						
-					}
+			$i = 0;
+			while(strlen($mediainfo) > 0) { 
+				$identifiers = array_values($identifiers);
+				
+			//	$mediainfo = str_replace("\x00\x00", "/", $mediainfo);
+				
+				/* 
+				** Tag frame
+				** Frame ID       $xx xx xx xx (four characters) 
+				** Size           $xx xx xx xx
+				** Flags          $xx xx
+				*/
+				$frameID  = substr($mediainfo, 0, 4);
+				$Size     = substr($mediainfo, 4, 4);
+				$Flags    = substr($mediainfo, 8, 1);
 					
+				for($i = 0; $i < 4; $i++) $Size[$i]  = ord($Size[$i]);
+				for($i = 0; $i < 1; $i++) $Flags[$i] = ord($Flags[$i]);
 					
-					if ($this->hasPrefix($mediainfo, 'C')) {
-						$frame = $this->parseComments($mediainfo);
-
-						$this->parsedheader['TAGS'][$frame['ID']] = $frame;
-						$mediainfo = substr($mediainfo, $frame['TOTALSIZE']);						
-					}
-
-					if ($this->hasPrefix($mediainfo, 'A')) {
-						$frame = $this->parseAttachmentHeader($mediainfo);
+				$Size = (int)$Size;
+				
+				
+			//	$Flags = (int)$Flags;
+			//	if ($frameID == "COMM") $Size += 10;
 					
-						$this->parsedheader['TAGS'][$frame['ID']] = $frame;
-				//		$mediainfo = substr($mediainfo, $frame['TOTALSIZE']);
-						break;						
-					}
-					
+				$StepSize = 10 + (int)$Size;
+				$Value    = substr($mediainfo, 10, (int)$Size);
+				
+				if (in_array($frameID, $identifiers)) {
+				
+					$this->parsedheader['TAGS'][$frameID] = array(
+						'TAG'   => $frameID,
+						'SIZE'  => $Size,
+						'FLAGS' => $Flags,
+						'DATA'  => $Value
+					);
+				
 				}
-			}
+				
+				$mediainfo = substr($mediainfo, $StepSize);
 			
-			if ($i == 500) break;
-			 $i++;
-		}
+				if ($i == count($identifiers)) break;
+			 	$i++;
+			}
 		
 			
 			$this->parsedheader['HEADER'] = $info;
